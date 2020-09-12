b096c3ba4c2184abfd51d4b908ae2a1b9f79d06b
cyq123450 <java_cyq@163.com>
2020-09-12 14:34:49 +0800

    喜淘金项目初始化工程

---
+++ src/test/java/com/cyq/money/EarnMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyq.money;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarnMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
